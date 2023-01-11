6309c9155f40c5cc86bbad1d571c544c230d4d8b
keaidaxiaohao <90555930+keaidaxiaohao@users.noreply.github.com>
2023-01-11 20:13:18 +0800

    Add files via upload

---
+++ onlineTV/js/main.js
@@ -0,0 +1,19 @@
+function playM3u8(url){
+  if(Hls.isSupported()) {
+      var video = document.getElementById('player');
+      video.volume = 1.0;
+      var hls = new Hls();
+      var m3u8Url = decodeURIComponent(url)
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+      });
+    }
+}
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
